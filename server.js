147a65b7b8dfab996fc74d155cb6d87759bf6c01
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2015-01-31 16:57:30 +0200

    jshint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var options = {
- key: fs.readFileSync('/path/of/file/ca.key'),
- cert: fs.readFileSync('/path/of/file/ca.crt'),
- ca: fs.readFileSync('/path/of/file/ca-bundle.crt'),
- requestCert: true,
- rejectUnauthorized: false
+  key: fs.readFileSync('/path/of/file/ca.key'),
+  cert: fs.readFileSync('/path/of/file/ca.crt'),
+  ca: fs.readFileSync('/path/of/file/ca-bundle.crt'),
+  requestCert: true,
+  rejectUnauthorized: false
 };
 
 var express = require('express');
